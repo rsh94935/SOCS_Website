8a01d4d90ef1cbfcccc197ee67a46fe443abf41a
rsh94935 <robert.s.hayward@hotmail.com>
2018-07-21 11:43:10 +0100

    Added dates to calendar

--- src/app/members/members.component.ts
+++ src/app/members/members.component.ts
@@ -58,6 +58,110 @@ export class MembersComponent implements OnInit {
       {
         "title": "May Half Term",
         "start": "2018-05-29"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-06-05"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-06-12"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-06-19"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-06-26"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-07-03"
+      },
+      {
+        "title": "July Summer Super Concert",
+        "start": "2018-07-07"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-07-10"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-07-17"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-09-04"
+      },
+      {
+        "title": "Sep AGM",
+        "start": "2018-09-11"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-09-18"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-09-25"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-10-02"
+      },
+      {
+        "title": "Choir Practice at Fulston 1300 to 1700",
+        "start": "2018-10-13"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-10-09"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-10-16"
+      },
+      {
+        "title": "Choir Practice at Fulston 1300 to 1700",
+        "start": "2018-10-21"
+      },
+      {
+        "title": "Choir Practice - venue to be booked",
+        "start": "2018-10-23"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-10-30"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-11-06"
+      },
+      {
+        "title": "Commemorative Concert at the Swallows 7:30pm",
+        "start": "2018-11-10"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-11-13"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-11-20"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-11-27"
+      },
+      {
+        "title": "Choir Practice",
+        "start": "2018-12-04"
+      },
+      {
+        "title": "Christmas Concert",
+        "start": "2018-12-08"
       }
     ];
   }
